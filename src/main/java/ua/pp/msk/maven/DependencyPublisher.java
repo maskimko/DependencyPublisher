1414d483dc99794453a30f8b9469adf283881d76
Maksym Shkolnyi <mshkolnyi@intropro.com>
2015-10-07 12:30:42 +0300

    Added license

--- src/main/java/ua/pp/msk/maven/DependencyPublisher.java
+++ src/main/java/ua/pp/msk/maven/DependencyPublisher.java
@@ -1,7 +1,17 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2015 Maksym Shkolnyi (aka maskimko)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
  */
 package ua.pp.msk.maven;
 
@@ -38,7 +48,7 @@ import ua.pp.msk.maven.exceptions.ArtifactPromotingException;
 
 /**
  *
- * @author maskimko
+ * @author Maksym Shkolnyi (aka maskimko)
  */
 @Mojo(name = "publish", defaultPhase = LifecyclePhase.INSTALL)
 public class DependencyPublisher extends AbstractMojo {
